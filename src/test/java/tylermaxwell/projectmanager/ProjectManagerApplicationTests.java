aad2080398f066510bdc38e545a28a3ca43b6c0d
tmax818 <tylermaxwell661@gmail.com>
2022-09-22 17:03:59 -0700

    init commit

---
+++ src/test/java/tylermaxwell/projectmanager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tylermaxwell.projectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
